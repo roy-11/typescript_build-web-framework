3cd05e2baeb1c0de7972b97295a1af071ba03b2f
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-05-17 15:06:21 +0900

    add on & trigger methods in UserClass

--- models/User.ts
+++ models/User.ts
@@ -2,8 +2,10 @@ type UserProps = {
   name?: string;
   age?: number;
 };
+type Callback = () => void;
 
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propsName: string): number | string {
@@ -12,5 +14,20 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback) {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string) {
+    const handlers = this.events[eventName];
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }
--- script.ts
+++ script.ts
@@ -9,3 +9,17 @@ console.log(user.get("age"));
 console.log(user.set({ name: "test2", age: 100 }));
 console.log(user.get("name"));
 console.log(user.get("age"));
+
+// on trigger test
+user.on("click", () => {
+  console.log("click! #1");
+});
+user.on("click", () => {
+  console.log("click! #2");
+});
+user.on("change", () => {
+  console.log("change! #1");
+});
+user.trigger("click");
+user.trigger("change");
+user.trigger("noname");
