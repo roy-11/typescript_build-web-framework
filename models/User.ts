e5d9db74ea9ded34f7a7676d5108b19c13a4377d
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-05-17 14:54:33 +0900

    add set method in UserClass

---
+++ models/User.ts
@@ -0,0 +1,16 @@
+type UserProps = {
+  name?: string;
+  age?: number;
+};
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propsName: string): number | string {
+    return this.data[propsName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
--- script.ts
+++ script.ts
@@ -1,12 +1,11 @@
-type UserProps = {
-  name: string;
-  age: number;
-};
+import { User } from "./models/User";
+const user = new User({ name: "test", age: 99 });
 
-export class User {
-  constructor(private data: UserProps) {}
+// get test
+console.log(user.get("name"));
+console.log(user.get("age"));
 
-  get(propsName: string): number | string {
-    return this.data[propsName];
-  }
-}
+// set test
+console.log(user.set({ name: "test2", age: 100 }));
+console.log(user.get("name"));
+console.log(user.get("age"));
